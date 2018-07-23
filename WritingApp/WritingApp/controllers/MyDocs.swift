89d262bcc0c95600bc034b24e65dbdbb427d6575
richard891216 <41025296+rich891216@users.noreply.github.com>
2018-07-23 16:06:38 -0700

    first UIs

---
+++ WritingApp/WritingApp/controllers/MyDocs.swift
@@ -0,0 +1,14 @@
+//
+//  MyDocs.swift
+//  WritingApp
+//
+//  Created by Richard Wang on 7/23/18.
+//  Copyright © 2018 Richard Wang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MyDocs: UIViewController {
+    
+}
